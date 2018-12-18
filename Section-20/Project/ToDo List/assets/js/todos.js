772e7ff74c46ead78611c3f62c81993fab9de62e
Arnab <arnab.sharma@nyalazone.com>
2018-12-18 15:44:47 +0200

    Deleting ToDos, stopping event bubbling and removing parent element

--- Section-20/Project/ToDo List/assets/js/todos.js
+++ Section-20/Project/ToDo List/assets/js/todos.js
@@ -3,3 +3,10 @@ $('li').on('click', function () {
     $(this).toggleClass('completed');
 });
 
+// Click on X to delete ToDo
+$('span').on('click', function (event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
